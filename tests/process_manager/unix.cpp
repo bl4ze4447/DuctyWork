575b95834cfabc8e24cf8852559da75da62ed632
bl4ze4447 <belutoni06@gmail.com>
2025-03-02 00:18:46 +0200

    Removed always_present for linux

--- tests/process_manager/unix.cpp
+++ tests/process_manager/unix.cpp
@@ -8,17 +8,8 @@
 
 TEST_CASE("Fill processes should add all running processes to the processes set", "[fill_process_unix]") {
     process_manager * pm = process_manager::create();
-    const std::string always_present[] = {
-        {"dbus-daemon"},
-        {"kthreadd"},
-        {"modprobe"}
-    };
-
     pm->fill_processes();
     const auto plist = pm->get_processes();
-
     REQUIRE(plist.empty() == false);
-    REQUIRE(std::ranges::any_of(plist.begin(), plist.end(), [&](const process & p) { return p.get_name() == always_present[0];}));
-    REQUIRE(std::ranges::any_of(plist.begin(), plist.end(), [&](const process & p) { return p.get_name() == always_present[1];}));
-    REQUIRE(std::ranges::any_of(plist.begin(), plist.end(), [&](const process & p) { return p.get_name() == always_present[2];}));
+    std::cout << plist[0].get_name() << std::endl;
 }
