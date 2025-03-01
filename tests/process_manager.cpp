595b50565722b1126c892060909a9ed4d8e058d3
bl4ze4447 <belutoni06@gmail.com>
2025-03-01 18:50:37 +0200

    Code typos

--- tests/process_manager.cpp
+++ tests/process_manager.cpp
@@ -8,9 +8,9 @@ TEST_CASE("Watching/unwatching a process should add/remove it to/from watched_pr
     process_manager * pm = process_manager::create();
     process test_proc(20, "test_proc.exe");
     pm->watch_process(test_proc);
-    REQUIRE(pm->get_watched_processes().contains(test_proc) == true)
+    REQUIRE(pm->get_watched_processes().contains(test_proc) == true);
 
     pm->unwatch_process(test_proc);
-    REQUIRE(pm->get_watched_processes().contains(test_proc) == false)
+    REQUIRE(pm->get_watched_processes().contains(test_proc) == false);
 }
 
