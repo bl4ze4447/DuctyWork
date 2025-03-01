0098526139c9739f1190ff4cb4946ec7e27c4a4b
bl4ze4447 <belutoni06@gmail.com>
2025-03-01 23:43:17 +0200

    fill_processes() was called incorrectly and the comparison logic was wrong in the process_manager tests

--- tests/process_manager/macosx.cpp
+++ tests/process_manager/macosx.cpp
@@ -4,19 +4,21 @@
 
 #include "../../process/process_manager.h"
 #include <catch2/catch_test_macros.hpp>
+#include <algorithm>
 
 TEST_CASE("Fill processes should add all running processes to the processes set", "[fill_process_macosx]") {
     process_manager * pm = process_manager::create();
-    const process always_present[] = {
-        {0, "cron"},
-        {0, "kthreadd"},
-        {0, "dbus-daemon"}
+    const std::string always_present[] = {
+        {"cron"},
+        {"kthreadd"},
+        {"dbus-daemon"}
     };
 
-    const auto plist = pm->get_processes();
     pm->fill_processes();
+    const auto plist = pm->get_processes();
+
     REQUIRE(plist.empty() == false);
-    REQUIRE(plist.contains(always_present[0]));
-    REQUIRE(plist.contains(always_present[1]));
-    REQUIRE(plist.contains(always_present[2]));
+    REQUIRE(std::ranges::any_of(plist.begin(), plist.end(), [&](const process & p) { return p.get_name() == always_present[0];}));
+    REQUIRE(std::ranges::any_of(plist.begin(), plist.end(), [&](const process & p) { return p.get_name() == always_present[1];}));
+    REQUIRE(std::ranges::any_of(plist.begin(), plist.end(), [&](const process & p) { return p.get_name() == always_present[2];}));
 }
--- tests/process_manager/win32.cpp
+++ tests/process_manager/win32.cpp
@@ -4,6 +4,7 @@
 
 #include "../../process/process_manager.h"
 #include <catch2/catch_test_macros.hpp>
+#include <algorithm>
 
 TEST_CASE("Fill processes should add all running processes to the processes set", "[fill_process_win32]") {
     process_manager * pm = process_manager::create();
@@ -13,10 +14,11 @@ TEST_CASE("Fill processes should add all running processes to the processes set"
         {0, "svchost.exe"}
     };
 
-    const auto plist = pm->get_processes();
     pm->fill_processes();
+    const auto plist = pm->get_processes();
+
     REQUIRE(plist.empty() == false);
-    REQUIRE(plist.contains(always_present[0]));
-    REQUIRE(plist.contains(always_present[1]));
-    REQUIRE(plist.contains(always_present[2]));
+    REQUIRE(std::ranges::any_of(plist.begin(), plist.end(), [&](const process & p) { return p.get_name() == always_present[0];}));
+    REQUIRE(std::ranges::any_of(plist.begin(), plist.end(), [&](const process & p) { return p.get_name() == always_present[1];}));
+    REQUIRE(std::ranges::any_of(plist.begin(), plist.end(), [&](const process & p) { return p.get_name() == always_present[2];}));
 }
