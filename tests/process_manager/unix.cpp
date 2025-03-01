d79649b160d0fa5f79013b9f1ce8238110b04599
bl4ze4447 <belutoni06@gmail.com>
2025-03-02 00:30:40 +0200

    use catch2 logging

--- tests/process_manager/unix.cpp
+++ tests/process_manager/unix.cpp
@@ -5,7 +5,6 @@
 #include "../../process/process_manager.h"
 #include <catch2/catch_test_macros.hpp>
 #include <algorithm>
-#include <iostream>
 
 TEST_CASE("Fill processes should add all running processes to the processes set", "[fill_process_unix]") {
     process_manager * pm = process_manager::create();
@@ -13,6 +12,7 @@ TEST_CASE("Fill processes should add all running processes to the processes set"
     const auto plist = pm->get_processes();
     REQUIRE(plist.empty() == false);
     for (const auto & p : plist) {
-        std::cout << p.get_name() << std::endl;
+        INFO(p.get_name() << '\n');
+        CHECK(FALSE);
     }
 }
